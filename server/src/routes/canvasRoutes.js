eddb2c522706e7cf4f83e361954a063ff9b6656c
aaditya3456 <rajaditya59385@gmail.com>
2026-09-21 01:18:36 +0530

    fix: forward async API failures to error middleware

--- server/src/routes/canvasRoutes.js
+++ server/src/routes/canvasRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { createCanvas, deleteCanvas, getCanvas, listCanvases, updateCanvas } from '../controllers/canvasController.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
 export const canvasRouter = Router();
-canvasRouter.route('/').post(createCanvas).get(listCanvases);
-canvasRouter.route('/:id').get(getCanvas).put(updateCanvas).delete(deleteCanvas);
+canvasRouter.route('/').post(asyncHandler(createCanvas)).get(asyncHandler(listCanvases));
+canvasRouter.route('/:id').get(asyncHandler(getCanvas)).put(asyncHandler(updateCanvas)).delete(asyncHandler(deleteCanvas));
